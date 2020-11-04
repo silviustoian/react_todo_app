cf0ff9bfe68f5c82c597b19d5d5e5f88893f5359
silviustoian <silviu.stoian96@gmail.com>
2020-11-04 17:08:52 +0200

    first commit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>TodoList</h1>
+            <Link to="/" style={linkStlye}>Home</Link> | <Link to="/about" style={linkStlye}>About</Link>
+        </header>
+    )
+}
+
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+}
+
+const linkStlye = {
+    color: '#fff',
+    textDecoration: 'none'
+}
+export default Header;
